6d8a48261eece9518ae13dbcf610f7f6fd5290f9
IvaT <snufkins.hat@gmail.com>
2024-04-14 05:17:00 +0200

    Update console logging

--- uu_app_maing01-hi/src/core/shoppingList.js
+++ uu_app_maing01-hi/src/core/shoppingList.js
@@ -32,22 +32,31 @@ const ShoppingList = createVisualComponent({
         {id: Utils.String.generateId(), name: "Mlíko", amount: "1 krabice"},
         {id: Utils.String.generateId(), name: "Sardinky", amount: "1 konzerva"}
     ]);
+ 
     const owner = USERS[2];
     const [members, setMembers] = useState([USERS[1], USERS[3]]);
 
-    const [open1, setOpen1] = useState(false);
-    const [open2, setOpen2] = useState(false);
-    const [open3, setOpen3] = useState(false);
+    const [openModalItem, setModalItem] = useState(false);
+    const [openModalMember, setModalMember] = useState(false);
+    const [openModalTitle, setModalTitle] = useState(false);
 
-    console.log(items);
+    //console.log(items);
 
     function removeItem(id) {
         setItems((items) => items.filter((item) => item.id !== id));
     }
     function removeMember(id) {
-        setMembers((members) => members.filter((member) => member.id !== id));
+        if (user.id === owner.id || user.id === id) {
+            setMembers((members) => members.filter((member) => member.id !== id));
+        } else {
+            console.log("Aktuální uživatel není vlastníkem nákupního seznamu ani odebíraným členem, nemůže tak odebrat jiného člena.");
+        }
     }
 
+    // nonMemberSelector - půjde do samostatné komponenty
+    const memberIDs = members.map((member) => member.id);
+    const nonMembers = USERS.filter((member) => member.id !== owner.id && !memberIDs.includes(member.id));
+    const nonMemberList = nonMembers.map(nonMember => ({value: nonMember.id, children: nonMember.name}));
 
     return (
         <>
@@ -55,9 +64,9 @@ const ShoppingList = createVisualComponent({
                 headerType = "title" 
                 card = "full" 
                 actionList = {[
-                    {icon: "uugds-basket", children: "Přidej položku", onClick: () => setOpen1(true) },
-                    {icon: "uugds-account", children: "Přidej člena", onClick: () => setOpen2(true) },
-                    {icon: "uugds-pencil", children: "Uprav název", onClick: () => setOpen3(true) }
+                    {icon: "uugds-basket", children: "Přidej položku", onClick: () => setModalItem(true) },
+                    {icon: "uugds-account", children: "Přidej člena", onClick: () => setModalMember(true) },
+                    {icon: "uugds-pencil", children: "Uprav název", onClick: () => setModalTitle(true) }
             ]}
             >
                 <div><b>Vlastník:</b> {owner.name}<hr /></div>
@@ -73,37 +82,31 @@ const ShoppingList = createVisualComponent({
                 </Uu5Elements.Grid>
 
                 <div>
-                    <hr />
-                    <br />
+                    <hr /><br />
                     <h3>Seznam položek:</h3>
                 </div>
 
                 <Uu5Elements.Grid>
                     {items.map((item) => (
                         <Item key={item.id} {...item} onRemove={() => removeItem(item.id)} />
-
-                    //  <div key={item.id}>
-                    //     {item.name}
-                    //  </div>
-
                     ))}
                 </Uu5Elements.Grid>
 
             </Uu5Elements.Block>
 
-            <Uu5Forms.Form.Provider key={open1} onSubmit={(e) => {
+            <Uu5Forms.Form.Provider key={openModalItem} onSubmit={(e) => {
                 const item = e.data.value;
                 setItems((items) => [...items, {...item, id: Utils.String.generateId() }]);
-                setOpen1(false);
+                setModalItem(false);
             }}>
                 <Uu5Elements.Modal 
-                    open={open1} 
-                    onClose={() => setOpen1(false)} 
+                    open={openModalItem} 
+                    onClose={() => setModalItem(false)} 
                     header="Nová nákupní položka"
                     footer={
                         <div className={Config.Css.css({display: "flex", gap: 8, justifyContent:"end"})}>
                             <Uu5Forms.SubmitButton />
-                            <Uu5Forms.CancelButton onClick={() => setOpen1(false)} />
+                            <Uu5Forms.CancelButton onClick={() => setModalItem(false)} />
                         </div>
                     }
                 >
@@ -114,18 +117,47 @@ const ShoppingList = createVisualComponent({
                 </Uu5Elements.Modal>
             </Uu5Forms.Form.Provider>
 
-            <Uu5Forms.Form.Provider key={open3} onSubmit={(e) => {
-                setShoppingListName(e.data.value.name);
-                setOpen3(false);
+            <Uu5Forms.Form.Provider key={openModalMember} onSubmit={(e) => {
+                const newMemberId = e.data.value.id;
+                if (user.id === owner.id) {
+                const newMember = nonMembers.find(nonMember => nonMember.id === newMemberId);
+                setMembers((members) => [...members, {...newMember}]);
+                } else {
+                    console.log("Aktuální uživatel není vlastníkem nákupního seznamu, a nemůže tak přidávat nové členy.");
+                }
+                setModalMember(false);
             }}>
                 <Uu5Elements.Modal 
-                    open={open3} 
-                    onClose={() => setOpen3(false)} 
+                    open={openModalMember} 
+                    onClose={() => setModalMember(false)} 
+                    header="Nový člen nákupního seznamu"
+                    footer={
+                        <div className={Config.Css.css({display: "flex", gap: 8, justifyContent:"end"})}>
+                            <Uu5Forms.SubmitButton />
+                            <Uu5Forms.CancelButton onClick={() => setModalMember(false)} />
+                        </div>
+                    }
+                >
+                    <Uu5Forms.FormTextSelect name="id" itemList={nonMemberList} required/>
+                </Uu5Elements.Modal>
+            </Uu5Forms.Form.Provider>
+
+            <Uu5Forms.Form.Provider key={openModalTitle} onSubmit={(e) => {
+                if (user.id === owner.id) {
+                    setShoppingListName(e.data.value.name);
+                } else {
+                    console.log("Aktuální uživatel není vlastníkem nákupního seznamu, a nemůže tak měnit jeho název.");
+                }
+                setModalTitle(false);
+            }}>
+                <Uu5Elements.Modal 
+                    open={openModalTitle} 
+                    onClose={() => setModalTitle(false)} 
                     header="Úprava názvu nákupního seznamu"
                     footer={
                         <div className={Config.Css.css({display: "flex", gap: 8, justifyContent:"end"})}>
                             <Uu5Forms.SubmitButton />
-                            <Uu5Forms.CancelButton onClick={() => setOpen3(false)} />
+                            <Uu5Forms.CancelButton onClick={() => setModalTitle(false)} />
                         </div>
                     }
                 >
